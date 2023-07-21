d605d3bd41edaa82104949f6ebf083959dcb19be
escotoj <j.escoto14@gmail.com>
2023-07-20 20:07:57 -0700

    add function to profile page and fetch set up for get_me

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,33 +1,57 @@
 import React from 'react';
 import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries'
+import { Typography, Paper, Box } from "@mui/material";
+
 
 const Profile = () => {
-  // Function to handle logout
-  const handleLogout = () => {
-    Auth.logout();
-  };
 
-  // Get the logged-in user's information
-  const user = Auth.getLoggedInUser();
 
-  return (
-    <div>
-      <h1>Profile</h1>
+  // route set up for when user is logged in
+  const { loading, error, data } = useQuery(GET_ME);
 
-      {user ? (
-        <>
-          <p>Welcome, {user.username}!</p>
-          <p>Email: {user.email}</p>
-          <button onClick={handleLogout}>Logout</button>
-        </>
-      ) : (
-        <>
-          <p>You are not logged in.</p>
-          <p>Please login to view your profile.</p>
-        </>
-      )}
-    </div>
-  );
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  const user = data.me;
+
+
+  // hardcode for texting
+  // const user = {
+  //   username: "JohnDoe",
+  //   email: "johndoe@example.com",
+  //   password: "xxxxxxxx",
+  // };
+
+
+
+return (
+  <Box sx={{ textAlign: "center", mt: 4 }}>
+    <Typography variant="h4" gutterBottom>
+      Profile
+    </Typography>
+
+    {user ? (
+      <Paper elevation={3} sx={{ p: 2, mt: 2 }}>
+        <Typography variant="body1">Username: {user.username}!</Typography>
+        <Typography variant="body1">Email: {user.email}</Typography>
+        <Typography variant="body1">Password: {user.password}</Typography>
+      </Paper>
+    ) : (
+      <Paper elevation={3} sx={{ p: 2, mt: 2 }}>
+        <Typography variant="body1">You are not logged in.</Typography>
+        <Typography variant="body1">Please login to view your profile.</Typography>
+      </Paper>
+    )}
+  </Box>
+);
 };
 
 export default Profile;
+
+
